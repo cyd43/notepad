73810a3765ca50d05db912ff0decb015b4476658
cyd <balloncyd43@gmail.com>
2026-08-16 20:27:05 +0800

    Update main.py
     .

--- main.py
+++ main.py
@@ -6,8 +6,37 @@ root = Tk()
 root.title("text editor")
 root.geometry("350x500")
 
+#save button function
+def new_file():
+    print("new_file")
 
-text=Text(root)
-text.grid()
+def open_file():
+    print("open_file")
+
+def save_file():
+    print("save_file")
+
+
+
+text=Text(root, height=50, width=43)
+text.grid(row=0, column=0)
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+filemenu = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="File", menu=filemenu)
+
+filemenu.add_command(label="New", command=new_file)
+filemenu.add_command(label="Open", command=open_file)
+filemenu.add_command(label="Save", command=save_file)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+
+helpmenu = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="About")
+
+#mga function
 
 root.mainloop()
